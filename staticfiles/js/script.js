2b9c12743b72007c507a233c806322ea7ff9b2d5
syler11 <nemeth.szilard82@gmail.com>
2023-08-18 19:40:31 +0000

    copied static files

---
+++ staticfiles/js/script.js
@@ -0,0 +1,11 @@
+// Display the current year in the footer
+var today = new Date();
+
+function getYear() {
+  var year = today.getFullYear();
+
+  document.getElementById("txtYear").innerHTML = year
+
+};
+
+getYear();

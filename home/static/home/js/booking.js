9874e23b4b20060c2502630a38954673b34ee0dd
syler11 <nemeth.szilard82@gmail.com>
2023-08-29 10:52:28 +0000

    added booking layout

--- home/static/home/js/booking.js
+++ home/static/home/js/booking.js
@@ -151,3 +151,14 @@ document.getElementById("day31").innerText = dayName31
 }
 
 changeContent()
+
+var nights = document.getElementById("nights").value;
+var rate_rm1 = document.getElementById("rate_rm1").innerHTML
+document.getElementById("rate_total").innerText = rate_rm1
+
+function changeBooking() {
+    var nights = document.getElementById("nights").value;
+    var rate_rm1 = document.getElementById("rate_rm1").innerHTML
+    total = parseInt(nights) * parseInt(rate_rm1)
+    document.getElementById("rate_total").innerText = total
+} 

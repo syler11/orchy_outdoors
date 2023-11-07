c8c6296eee7230ae4479c592e5690170a83bd737
syler11 <nemeth.szilard82@gmail.com>
2023-11-07 18:00:59 +0000

    added google map

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -10,3 +10,20 @@ function getYear() {
 
 getYear();
 
+function initMap() {
+  var location = {
+    lat: 56.51734880123457,
+    lng: -4.765033244920148
+  }; // Replace with your coordinates
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15, // Adjust the zoom level as needed
+    center: location
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Bridge of Ochy Pods' // Replace with your location name
+  });
+}
+
+initMap()

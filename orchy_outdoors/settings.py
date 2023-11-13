1f4b3468b7eb1facd0eb9f4f49f8e99c2e952fcf
syler11 <nemeth.szilard82@gmail.com>
2023-11-13 14:54:59 +0000

    debug changed to false

--- orchy_outdoors/settings.py
+++ orchy_outdoors/settings.py
@@ -168,6 +168,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+SITE_NAME = 'Orchy Outdoors'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
